aef4c539120f1824b811c264e58c00c0980d0661
AGROYGBV <amandagracerenfro@gmail.com>
2022-02-03 17:07:38 -0600

    added some initial react

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import './api/dogs'
-import {useEffect} from 'react'
+import {Component, useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return <Component {...pageProps} />
+
 }
 
 export default MyApp
