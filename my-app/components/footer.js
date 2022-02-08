c038f983f9c4094b1da1d4537aab37ec3a21c4b3
Lamin Suso <susolamin@hotmail.com>
2022-02-08 09:46:49 -0600

    Updated landing page

---
+++ my-app/components/footer.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+        <p class="text-center text-muted">© 2021 Company, Inc</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,53 +1,102 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import App from './_app'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import App from "./_app";
+import Footer from "../components/footer";
+
 export default function Home() {
   return (
     <div>
-
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="allDogs">All Dogs</a></li>
-    <li><a class="dropdown-item" href="dogProfile">Dog Profile</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
-
-
-<main>
-  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
-      <h1 class="display-4 fw-normal">Punny headline</h1>
-      <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-      <a class="btn btn-outline-secondary" href="#">Coming soon</a>
-    </div>
-    <div class="product-device shadow-sm d-none d-md-block"></div>
-    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-  </div>
-
-  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-    <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-      <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Dropdown button
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li>
+            <a class="dropdown-item" href="allDogs">
+              All Dogs
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="dogProfile">
+              Dog Profile
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
       </div>
-      <div class="bg-light shadow-sm mx-auto"></div>
+
+      <main>
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+          <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 fw-normal">Punny headline</h1>
+            <img
+              src={"img/dog-banner.png"}
+              class="img-fluid"
+              alt="Responsive image"
+            ></img>
+            <p class="lead fw-normal">
+              And an even wittier subheading to boot. Jumpstart your marketing
+              efforts with this example based on Apple’s marketing pages.
+            </p>
+            <a class="btn btn-outline-secondary" href="#">
+              Coming soon
+            </a>
+          </div>
+          <div class="product-device shadow-sm d-none d-md-block"></div>
+          <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        </div>
+
+        <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+              <h2 class="display-5">Another headline</h2>
+              <img src={"img/RockyandMe.jpg"} width="300" height="200"></img>
+              <p class="lead">And an even wittier subheading.</p>
+            </div>
+            <div class="bg-light shadow-sm mx-auto"></div>
+          </div>
+          <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+              <h2 class="display-5">Another headline</h2>
+              <img src={"img/ctor.svg"} width="300" height="200"></img>
+              <p class="lead">And an even wittier subheading.</p>
+            </div>
+            <div class="bg-dark shadow-sm mx-auto"></div>
+          </div>
+          <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+              <h2 class="display-5">Another headline</h2>
+              <img src={"img/howie.svg"} width="300" height="200"></img>
+              <p class="lead">And an even wittier subheading.</p>
+            </div>
+            <div class="bg-dark shadow-sm mx-auto"></div>
+          </div>
+          <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+              <h2 class="display-5">Another headline</h2>
+              <img src={"img/alvan.svg"} width="300" height="200"></img>
+              <p class="lead">And an even wittier subheading.</p>
+            </div>
+            <div class="bg-light shadow-sm mx-auto"></div>
+          </div>
+        </div>
+      </main>
+      <br></br>
+      <br></br>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-      <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-      <div class="bg-dark shadow-sm mx-auto"></div>
-    </div>
-  </div>
-</main>
-    </div>
-  )
+  );
 }
-
-
