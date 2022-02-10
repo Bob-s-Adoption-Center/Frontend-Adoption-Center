96c8f38c26b216d2b24e67c77ccb7078f9f8a89e
AGROYGBV <amandagracerenfro@gmail.com>
2022-02-10 15:39:26 -0600

    added form

--- my-app/pages/adoptForm.jsx
+++ my-app/pages/adoptForm.jsx
@@ -1,11 +1,13 @@
 import React, { useState, Component, Fragment } from 'react';
-import Form from 'react-bootstrap/Form'
+import {Form, Row, Col, Button} from 'react-bootstrap'
+// import Jumbotron from 'react-bootstrap/Jumbotron'
+
 
 
 function adoptForm(props) {
 
     //set an initial state for the form
-    const initialState = {name:"", image:""}
+    const initialState = {name:""}
 
     //store the sauce form in state
     const [formState, setFormState]= useState(initialState)
@@ -21,106 +23,140 @@ function adoptForm(props) {
         e.preventDefault()
 
         //create new sauce in database
-        postSauce()
+        postAdopt()
 
         //reset form
         setFormState(initialState)
     }
 
     //create new sauce in database
-    const postSauce = async () => {
-        await fetch(`http://localhost:3000/new-sauce`, {
+    const postAdopt = async () => {
+        await fetch(`http://localhost:3000/adoptForm`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 name: formState.name,
-                image: formState.image
+                
             })
         })
-        window.location.assign('/dogs')
+        window.location.assign('/adoptForm')
     }
 
 
     return (
         <>
-        {/* <h2>How may we contact you?</h2>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="fname">First Name:</label>
-            <input type="text" id="fname" name="fname" required onChange={handleChange}/><br/>
-            <label htmlFor="lname">Last Name:</label>
-            <input type="text" id="lname" name="lname" required onChange={handleChange}/><br/>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required onChange={handleChange}/><br/>
-            <label htmlFor="phone">Phone:</label>
-            <input type="tel" id="phone" name="phone" required onChange={handleChange}/><br/>
-            <label htmlFor="address">Address:</label>
-            <input type="text" id="address" name="address" required onChange={handleChange}/><br/>
-            <label htmlFor="city">City:</label>
-            <input type="text" id="city" name="city" required onChange={handleChange}/><br/>
-            <label htmlFor="state">State:</label>
-            <input type="text" id="state" name="state" required onChange={handleChange}/><br/>
-            <label htmlFor="zip">Zip:</label>
-            <input type="text" id="zip" name="zip" required onChange={handleChange}/><br/>
-            <label htmlFor="birthday">Birthday:</label>
-            <input type="date" id="birthday" name="birthday" required onChange={handleChange}/><br/>
-
-            <button type="submit">Submit Adoption Application</button>
-        </form> */}
-        <Form>
-            <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-            </Row>
-
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-
+        {/* <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+            </p>
+            <p>
+                <Button bsStyle="primary">Learn more</Button>
+            </p>
+        </Jumbotron>; */}
+        <Form className="adoptForm">
+        <div className="formSection">
+        <h3>Contact Information</h3>
+        <p3>Complete the questionaire below to begin this adoption inquiry.</p3>
+        <br/>
+        <br/>
         <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-            </Form.Group>
+    <Form.Group as={Col} controlId="formGridFname">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="text" placeholder="First Name" />
+    </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Select defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
-            </Form.Select>
-            </Form.Group>
+    <Form.Group as={Col} controlId="formGridLname">
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control type="text" placeholder="Last Name" />
+    </Form.Group>
+  </Row>
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-            </Form.Group>
-        </Row>
+    <Form.Group as={Col} controlId="formGridBirthday">
+      <Form.Label>Birthday</Form.Label>
+      <Form.Control type="date" placeholder="" />
+    </Form.Group>
+  </Row>
 
-        <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+  <Form.Group className="mb-3" controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
 
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
+  <Form.Group className="mb-3" controlId="formPhone">
+    <Form.Label>Phone Number</Form.Label>
+    <Form.Control placeholder="214.222.3333" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Select defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Select>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Row>
+
+  </div>
+
+  <div className="formSection">
+  <Row className="mb-3">
+  <h3>Background Check Information</h3>
+  <p3>Verifiable ID required for adoption process.</p3>
+  <br/>
+  <br/>
+    <Form.Group as={Col} controlId="formGridDocID">
+      <Form.Label>Document ID #</Form.Label>
+      <Form.Control type="text" placeholder="Enter liscense ID" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>Issuing State</Form.Label>
+      <Form.Select defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Select>
+    </Form.Group>
+  </Row>
+
+  </div>
+
+  <Form.Group className="mb-3" id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check for Adoption Updates" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
 </Form>
+
         </>
     );
 }
 
 export default adoptForm;
+
