bdafc002d161f8498569daec4363d9b3d0b198fa
Martin Renteria <renteriamp96@gmail.com>
2022-06-21 13:18:04 -0700

    Removing sign in from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./api/oauth-token";
 import { useEffect, useState, createContext } from "react";
-import SignIn  from './signin'
 export const AuthContext = createContext();
 
 function MyApp({ Component, pageProps }) {
@@ -20,9 +19,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <AuthContext.Provider value={accessToken}>
-      <SignIn>
       <Component {...pageProps} />
-      </SignIn>
     </AuthContext.Provider>
   );
 }
